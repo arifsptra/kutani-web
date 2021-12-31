3112f8aa4abac67c156762533a00c12a437baecc
arifsptra <111202113480@mhs.dinus.ac.id>
2021-12-31 14:00:01 +0700

    Slideshow view upgrade

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-var slides = document.querySelectorAll("#slides .slide");
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 4000);
+// var slides = document.querySelectorAll("#slides .slide");
+// var currentSlide = 0;
+// var slideInterval = setInterval(nextSlide, 4000);
 
-function nextSlide() {
-  slides[currentSlide].className = "slide";
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].className = "slide showing";
-}
+// function nextSlide() {
+//   slides[currentSlide].className = "slide";
+//   currentSlide = (currentSlide + 1) % slides.length;
+//   slides[currentSlide].className = "slide showing";
+// // }
+$(document).ready(function(){
+  $('.slides').slick({
+      autoplay: true,
+      autoplaySpeed: 2500,
+      dots: true
+  });
+});
